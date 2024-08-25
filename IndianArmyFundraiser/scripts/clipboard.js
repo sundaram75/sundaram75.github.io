576931e2124b9d3a826d53c8645f3318b7b1f5da
sundaram75 <sundaram.p.75@gmail.com>
2024-08-25 12:58:29 +0530

    1. Added faillure condition 2.Used button instead of link

--- IndianArmyFundraiser/scripts/clipboard.js
+++ IndianArmyFundraiser/scripts/clipboard.js
@@ -1,13 +1,19 @@
 // Add your JavaScript code here
 document.addEventListener('DOMContentLoaded', function() {
     // Get the copy link element
-    var copyLink = document.getElementById('copy-link');
+    var copyLink = document.getElementById('copy-button');
 
     // Initialize clipboard.js
     new ClipboardJS(copyLink);
 
-    // Add event listener for successful copy
+    // Add event listener for  copy
     copyLink.addEventListener('click', function() {
-        alert('Text copied to clipboard!');
+        var clipboard = new ClipboardJS(copyLink);
+        clipboard.on('success', function(e) {
+            alert('UPI ID is copied to your clipboard!');
+        });
+        clipboard.on('error', function(e) {
+            alert('Sorry, UPI ID could not be copied to your clipboard.');
+        });
     });
 });
